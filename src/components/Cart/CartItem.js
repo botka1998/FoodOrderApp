55d2113249de30607e90c470afd00d75ca10b142
AndrejBotka <andrej.botka@sigmoid.rs>
2022-12-09 15:00:42 +0100

    add CartItem

---
+++ src/components/Cart/CartItem.js
@@ -0,0 +1,31 @@
+import React, { useContext } from "react";
+import styles from "./CartItem.module.css";
+import CartContext from "../../store/cart-context";
+
+const CartItem = (props) => {
+  const ctx = useContext(CartContext);
+  const onRemoveHandler = () => {
+    ctx.removeItem({ name: props.name, amount: 1 });
+  };
+  const onAddHandler = () => {
+    ctx.addItem({ name: props.name, amount: 1 });
+  };
+  const price = `$${props.price.toFixed(2)}`;
+  return (
+    <li className={styles["cart-item"]}>
+      <div>
+        <h2>{props.name}</h2>
+        <div className={styles.summary}>
+          <span className={styles.price}>{price}</span>
+          <span className={styles.amount}>x {props.amount}</span>
+        </div>
+      </div>
+      <div className={styles.action}>
+        <button onClick={onRemoveHandler}>-</button>
+        <button onClick={onAddHandler}>+</button>
+      </div>
+    </li>
+  );
+};
+
+export default CartItem;
